dce9c7f73447ba403f4571e46d64b54a5c9f686f
connoisseures <connoisseures@gmail.com>
2024-07-07 23:41:18 +0000

    yf loader

--- qstrader/data/yfloader.py
+++ qstrader/data/yfloader.py
@@ -1,8 +1,11 @@
+from typing import List
+
 import yfinance as yf
 
 
 class Yfloader:
-    def load_as_csv(self, start_date, end_date, ticker):
-        data = yf.download(ticker, start_date, end_date)
-        # Export data to a CSV file
-        data.to_csv(f"{ticker}.csv")
+    def load_as_csv(self, start_date, end_date, ticker: List[str]):
+        for tk in ticker:
+            data = yf.download(tk, start_date, end_date)
+            # Export data to a CSV file
+            data.to_csv(f"{ticker}.csv")
